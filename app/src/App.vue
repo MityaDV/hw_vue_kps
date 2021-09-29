3f1533cda5b015293535da34babe24b8fc4f9e49
MityaDv <mityadv@gmail.com>
2021-09-29 22:26:33 +0300

    add component SearchForm

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,19 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+    <SearchForm />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SearchForm from "@/components/SearchForm.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    SearchForm,
+  },
+};
 </script>
 
 <style lang="scss">
---
+++ app/src/components/SearchForm.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h2>{{ title }}</h2>
+    <input type="text" />
+    <label></label> <br />
+    <button>{{ btnTitle }}</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SearchForm",
+  data() {
+    return {
+      title: "Search Form",
+      btnTitle: "Отправить",
+    };
+  },
+};
+</script>
