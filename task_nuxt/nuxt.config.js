48fdc9f4d90ef622e67b3ee8dd5305bf9bf45cb5
MityaDv <mityadv@gmail.com>
2021-10-18 22:52:54 +0300

    add page index

--- task_nuxt/nuxt.config.js
+++ task_nuxt/nuxt.config.js
@@ -6,7 +6,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - task_nuxt',
+    titleTemplate: '%s',
     title: 'task_nuxt',
     htmlAttrs: {
       lang: 'en',
@@ -50,7 +50,7 @@ export default {
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
     theme: {
-      dark: true,
+      dark: false,
       themes: {
         dark: {
           primary: colors.blue.darken2,
---
+++ task_nuxt/pages/cart.vue
@@ -0,0 +1,7 @@
+<template>
+  <v-row>
+    <v-col class="text-center">
+      <v-btn to="/" depressed color="primary"> Primary </v-btn>
+    </v-col>
+  </v-row>
+</template>
