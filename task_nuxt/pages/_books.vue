e84bbf89e63532bfe02d10399d371ffb565eba16
MityaDv <mityadv@gmail.com>
2021-11-17 17:31:06 +0200

    nuxt +

---
+++ task_nuxt/pages/_books.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-row justify="center">
+    <v-col justify="center" align="center" cols="12" md="8">
+      <h2>Search books</h2>
+      <p>query for this page: {{ $route.query }}</p>
+      <p>env: {{ env }}</p>
+      <nuxt-link to="/">Home</nuxt-link>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'SearchBooksPage',
+  data() {
+    return {
+      env: process.env.BASE_APP_URL,
+    }
+  },
+}
+</script>
--- task_nuxt/pages/index.vue
+++ task_nuxt/pages/index.vue
@@ -20,23 +20,19 @@
           required
         >
         </v-text-field>
-        <v-btn color="primary" elevation="3" @click="fetchBooks">
+        <nuxt-link to="_books" tag="button">
           {{ form.btnTitle }}
-        </v-btn>
+        </nuxt-link>
       </v-form>
     </v-col>
-
-    <!-- {{ $store.state }} -->
   </v-row>
 </template>
 
 <script>
 export default {
   name: 'HomePage',
-  // layout: 'SearchBooks',
   data() {
     return {
-      items: [],
       form: {
         searchText: '',
         btnTitle: 'Отправить',
@@ -45,23 +41,6 @@ export default {
       },
     }
   },
-  methods: {
-    fetchBooks() {
-      const initItems = [1, 2, 3, 4, 5, 6, 7]
-      this.items = []
-
-      fetch(this.$config.baseURL + this.form.searchText, {
-        method: 'GET',
-      })
-        .then((response) => response.json())
-        .then((json) => {
-          for (const i in initItems) {
-            const it = json.items[i]
-            this.items.push(it)
-          }
-        })
-      this.form.searchText = ''
-    },
-  },
+  methods: {},
 }
 </script>
